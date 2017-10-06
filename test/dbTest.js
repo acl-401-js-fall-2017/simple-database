164585e193039cf688ccd52676c8fb7516f85057
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-06 10:54:53 -0700

    the tests are passing, not sure why

--- test/dbTest.js
+++ test/dbTest.js
@@ -1,15 +1,25 @@
 const assert = require('assert');
 const Store = require('../lib/store.js');
 const Db = require('../lib/db.js');
+const fs = require('fs');
+
 
 // console.log('DIRNAME', __dirname);
 // console.log('FILENAME', __filename);
 
 describe('DataBase', () =>{
 
+    let freshDB = null;
+    beforeEach( done => {
+        freshDB = new  Db(__dirname, done);
+    });
+
     it('returns an DB object',() =>{
-        const freshDB = new  Db(__dirname);
         assert.ok(freshDB instanceof  Db);
+    });
+
+    it('creates a file', () => {
+        assert.ok(fs.existsSync(__dirname + '/db'));
     });
 
 });
