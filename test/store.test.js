8db1d432070934b53069d7b3dc2a4d8239c677e1
mugworks <greenwood.pdx@gmail.com>
2017-10-06 10:16:31 -0700

    test passed - made a new store

--- test/store.test.js
+++ test/store.test.js
@@ -1,8 +1,16 @@
 const assert = require('assert');
 
-
-
+class Store {
+    constructor() {
+        this.list = [];
+    }
+}
+let store = null;
 describe('make store', () => {
+
+    beforeEach(function() {
+        store = new Store();
+    });
 
     it('should make a new store', () => {
         assert.ok(store);
