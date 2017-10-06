d7e15e7918faccffb3fe5cd1ca34feb2e3f68574
mugworks <greenwood.pdx@gmail.com>
2017-10-06 10:27:38 -0700

    test passes - create an id

--- test/store.test.js
+++ test/store.test.js
@@ -1,18 +1,31 @@
 const assert = require('assert');
+const shortid = require('shortid');
 
 class Store {
     constructor() {
         this.list = [];
     }
+    save(obj) {
+        obj._id = shortid.generate();
+    }
 }
 let store = null;
+let obj1 = null;
 describe('make store', () => {
 
     beforeEach(function() {
         store = new Store();
+        obj1 = {
+            name: 'dog'
+        };
     });
 
     it('should make a new store', () => {
         assert.ok(store);
+    });
+
+    it('should create an id property for the object', () => {
+        store.save(obj1);
+        assert.deepEqual(typeof obj1._id, 'string');
     });
 });
