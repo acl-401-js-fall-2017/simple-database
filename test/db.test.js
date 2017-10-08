7f8462ca5f9000f7d7d3b957cf21c282fe765c6c
Zac-L <Zachlichens@gmail.com>
2017-10-08 13:08:08 -0700

    eslint disabled a line to get test running

---
+++ test/db.test.js
@@ -0,0 +1,21 @@
+// eslint-disable-next-line
+const assert = require('assert');
+// eslint-disable-next-line
+const MakeDb = require('../lib/db.js');
+
+
+describe('db test functions', () =>{
+    // eslint-disable-next-line
+    let db = null;
+
+    beforeEach( () => {
+        db = new MakeDb;
+    });
+
+    describe('Save', () => {
+        it('Saves File Name', () =>{
+
+        });
+    });
+
+});
