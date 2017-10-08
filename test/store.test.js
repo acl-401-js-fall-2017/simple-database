373d2bd3031656e46c2ccd819166b2fdbaac892a
ckrountree <jmail0207@gmail.com>
2017-10-08 14:10:23 -0700

    write and pass remove method

--- test/store.test.js
+++ test/store.test.js
@@ -7,7 +7,7 @@ const path = require('path');
 describe.only('create storeDir name', () => {
     let store = null;
     const testDir = path.join(__dirname, 'data');
-
+    
     beforeEach(done => {
         rimraf(testDir, err => {
             if(err) return done(err);
@@ -18,17 +18,34 @@ describe.only('create storeDir name', () => {
             });
         });
     });
-
+    
     it('gets a saved obj', done => {
         const obj = {name: 'Kate'};
+
         store.save(obj, (err, savedObj) => {
             if(err) return done(err);
             assert.ok(savedObj._id);
             assert.equal(savedObj.name, obj.name);
 
+            // .get reads the contents of this file
             store.get(savedObj._id, (err, gotObjWithId) => {
                 if(err) return done(err);
                 assert.deepEqual(gotObjWithId, savedObj);
+                done();
+            });
+        });
+    });
+
+    it('removes files by id', done => {
+        const obj = {name: 'Kate'};
+        
+        store.save(obj, (err, savedObj) => {
+            if(err) return done(err);
+            assert.ok(savedObj._id);
+            assert.equal(savedObj.name, obj.name);
+
+            store.remove(obj._id, (err, removedObj) => {
+                assert.deepEqual(removedObj, { removed: true });
                 done();
             });
         });
