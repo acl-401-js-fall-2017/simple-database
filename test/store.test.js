421074d6cecd2d4da97ae173a1b9233dd85a15be
Zac-L <Zachlichens@gmail.com>
2017-10-08 15:29:43 -0700

    created test for getAll

--- test/store.test.js
+++ test/store.test.js
@@ -7,8 +7,9 @@ const Store = require('../lib/store');
 describe.only('creating store dir', () => {
     let store = null;
     const testDir = path.join(__dirname, 'test-file');
-    const testObject = {name: 'Zac'};
-
+    
+    let testFile;
+    
     beforeEach( done => {
         rimraf( testDir, err => {
             if(err) return done(err);
@@ -19,14 +20,15 @@ describe.only('creating store dir', () => {
             });
         });
     });
-
-    it('gets a saved obj', done => {
-
+    
+    it('saving and getting the saved obj', done => {
+        const testObject = {name: 'Zac'};
+        
         store.save(testObject, (err, savedObject) => {
             if(err) return done(err);
             assert.ok(savedObject._id);
             assert.equal(savedObject.name, testObject.name);
-
+            
             store.get(savedObject._id, (err, objWithId) => {
                 if(err) return done(err);
                 assert.deepEqual(objWithId, savedObject);
@@ -36,10 +38,34 @@ describe.only('creating store dir', () => {
     });
 
     it('removed file by id', done => {
-        store.remove(testObject._id, (err, removedObj) => {
-            assert.deepEqual(removedObj, { removed: true } );
-            done();
-        });
+        const testObject = {name: 'Zac'};
 
+        store.save(testObject, (err, savedObject) => {
+            if(err) return done(err);
+            assert.ok(savedObject._id);
+            assert.equal(savedObject.name, testObject.name);
+
+            store.remove(testObject._id, (err, removedObj) => {
+                assert.deepEqual(removedObj, { removed: true } );
+                done();
+            });
+
+        });
+    });
+
+    it('gets all objects', done => {
+        const testObject = {name: 'Zac'};
+
+        store.save(testObject, (err, savedObject) => {
+            if(err) return done(err);
+            assert.ok(savedObject._id);
+            assert.equal(savedObject.name, testObject.name);
+
+            store.getAll((err, files) => {
+                if(err) return done(err);
+                assert.equal(files.length, 1);
+                done();
+            });
+        });
     });
 });
