80365708a7bd932da50d940b51af4b52073d6eed
Andrew Bodey <a.s.bodey@gmail.com>
2017-10-09 12:30:36 -0700

    fixed tests and methods for remove stores

--- test/storeTest.js
+++ test/storeTest.js
@@ -22,13 +22,13 @@ describe('Stores', () => {
             } else {
                 myStore = new Store(storeName);
             }
-            myStore.save(thing, (err , newObj) => {
-                if(err) console.error(err);
-                thingId = newObj._id;
-            });
-            for(let i= 10000; i >0; i--){
-                //nothing to see here, move along
-            }
+            // myStore.save(thing, (err , newObj) => {
+            //     if(err) console.error(err);
+            //     thingId = newObj._id;
+            // });
+            // for(let i= 100000; i >0; i--){
+            //     //nothing to see here, move along
+            // }
         });
 
         it('saves a new object and assigns it an ID', done => {
@@ -42,40 +42,55 @@ describe('Stores', () => {
         });
 
         it('gets the object with thing ID', done =>{
-            let gotten = null;
-            myStore.get(thingId, (error, newObj) =>{
-                if(error){
-                    return done(error);
-                }
-                gotten = newObj;
-                assert.deepEqual(gotten, {  name: 'thing', description: 'a thing' , _id: gotten._id });
-                done();
+            myStore.save(thing, (err, newObj) => {
+                if (err) console.error(err);
+                thingId = newObj._id;
+                let gotten = null;
+                myStore.get(thingId, (error, newObj) =>{
+                    if(error){
+                        return done(error);
+                    }
+                    gotten = newObj;
+                    assert.deepEqual(gotten, {  name: 'thing', description: 'a thing' , _id: gotten._id });
+                    done();
+                });
             });
         });
 
-        it('removes object with given ID', done =>{
-            let removed = null;
-            myStore.remove(thingId, (error,status) => {
-                if (error) {
-                    return done(error);
-                }
-                removed = status;
-                assert.deepEqual(removed, {removed : true});
-                done();
+        it('removes object with given ID {removed : true}', done =>{
+            myStore.save(thing, (err, newObj) => {
+                if (err) console.error(err);
+                thingId = newObj._id;
+                let removed = null;
+    
+                myStore.remove(thingId, (error,status) => {
+                    if (error) {
+                        return done(error);
+                    }
+                    removed = status;
+                    assert.deepEqual(removed, {removed : true});
+                    done();
+                });
             });
+
         });
 
-        it('removes object with given ID', done => {
-            let removed = null;
-            myStore.remove('bad ID', (error, status) => {
-                if (error) {
-                    return done(error);
-                }
-                removed = status;
-                assert.deepEqual(removed, { removed: false });
-                done();
+        it('removes object with given ID { removed: false }', done => {
+            myStore.save(thing, (err, newObj) => {
+                if (err) console.error(err);
+                thingId = newObj._id;
+                let removed = null;
+                myStore.remove('bad ID', (error, status) => {
+                    if (error) {
+                        return done(error);
+                    }
+                    removed = status;
+                    assert.deepEqual(removed, { removed: false });
+                    done();
+                });
             });
         });
-
     });
+
+
 });
