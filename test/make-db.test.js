8a8fdc4e3f1f97f6cc96a06b360dc76576635423
ckrountree <jmail0207@gmail.com>
2017-10-09 10:23:32 -0700

    write and pass database getStore method

--- test/make-db.test.js
+++ test/make-db.test.js
@@ -1,3 +1,27 @@
-/* eslint-disable*/
 const assert = require('assert');
-const mkdirp = require('mkdirp');
+const Db = require('../lib/make-db');
+const rimraf = require('rimraf');
+const path = require('path');
+const Store = require('../lib/store');
+
+describe.only('create root directories', () => {
+    let db = null;
+    const testRootDir = path.join(__dirname, 'data');
+
+    beforeEach(done => {
+        rimraf(testRootDir, err => {
+            if(err) return done(err);
+            db = new Db(testRootDir);
+            done();
+
+        });
+    });
+
+    it('checks for instances of Store', done => {
+        db.getStore('storeName', (err, checkedStore) => {
+            if (err) return (err);
+            assert.ok(checkedStore instanceof Store);
+            done();
+        });
+    });
+});
