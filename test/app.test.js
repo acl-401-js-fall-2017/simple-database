c2cb42363885290d3fa1395d0c1be6b061bc2282
David Nishikawa <triptype@gmail.com>
2017-10-12 10:02:34 -0700

    write red test for save method

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,13 @@
-'use strict';
 
 const app = require('../lib/app');
+const {promisify} = require('util');
 const chai = require('chai');
 const assert = chai.assert;
 const rimraf = require('rimraf');
+const path = require('path');
 const Store = require('../lib/Store');
 
-const storeDir = __dirname + '/testStore';
+const storeDir = path.join(__dirname, 'testStore');
 
 
 describe('Store:', () => {
@@ -16,8 +17,29 @@ describe('Store:', () => {
             assert.ok(newStore instanceof Store);
         });
     });
-
-    describe('save method', () => {
+    
+    describe('store methods', () => {
         
+        let newStore = null;
+        let lionKing = null;
+        let mulan = null;
+        let moana = null;
+        beforeEach(() => {
+            newStore = new Store(storeDir);
+            lionKing = { title: 'the Lion King', year: '1994' };
+            mulan = { title: 'Mulan', year: '1998' };
+            moana = { title: 'Moana', year: '2016'};
+        });
+
+        describe('save method', () => {
+            it('gives the object an _id/filename and saves it to a file', () => {
+                const jungleBook = { title: 'the Jungle Book', year: '1967' };
+                newStore.save(jungleBook)
+                    .then(savedObj => {
+                        assert.deepEqual(savedObj, { title: 'the Jungle Book', year: '1967', id: savedObj._id });
+                    });
+            });
+        });
     });
+
 });
