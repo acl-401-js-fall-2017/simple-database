5ded3fbe6c9c3b8aaaa42e480992c567b667d9e9
Zac-L <Zachlichens@gmail.com>
2017-10-12 10:55:12 -0700

    updated tests to use promises

--- test/store.test.js
+++ test/store.test.js
@@ -1,69 +1,92 @@
 const assert = require('assert');
-const rimraf = require('rimraf');
-const mkdirp = require('mkdirp');
 const path = require('path');
+const { rimraf, mkdirp } = require('../lib/promisified');
+
 const Store = require('../lib/store');
 
 describe('creating store dir', () => {
     let store = null;
     const testDir = path.join(__dirname, 'test-file');
     
-    beforeEach( done => {
-        rimraf( testDir, err => {
-            if(err) return done(err);
-            mkdirp( testDir, err => {
-                if(err) return done(err);
-                store = new Store(testDir);
-                done();
-            });
-        });
+    beforeEach( () => {
+        return rimraf( testDir )
+            .then( () => mkdirp(testDir))
+            .then( () => store = new Store(testDir));
     });
     
-    it('saving and getting the saved obj', done => {
-        const testObject = {name: 'Zac'};
-        
-        store.save(testObject, (err, savedObject) => {
-            if(err) return done(err);
-            assert.ok(savedObject._id);
-            assert.equal(savedObject.name, testObject.name);
-            
-            store.get(savedObject._id, (err, objWithId) => {
-                if(err) return done(err);
+    it('saving and getting the saved obj', () => {
+        let savedObject = null;
+        let testObject = {name: 'Zac'};
+
+        store.save(testObject)
+            .then(_savedObject => {
+                savedObject = _savedObject;
+
+                assert.ok(savedObject._id);
+                assert.equal(savedObject.name, testObject.name);
+
+                return store.get(savedObject._id);
+            })
+            .then(objWithId => {
                 assert.deepEqual(objWithId, savedObject);
-                done();
+
             });
-        });
     });
 
-    it('removed file by id', done => {
+    it('returns null for bad id', () => {
+        return store.get('bad id')
+            .then((item) => {
+                assert.strictEqual(item, null);
+            });
+    });
+
+    it('removed file by id', () => {
         const testObject = {name: 'Zac'};
+        let savedObject = null;
 
-        store.save(testObject, (err, savedObject) => {
-            if(err) return done(err);
-            assert.ok(savedObject._id);
-            assert.equal(savedObject.name, testObject.name);
-
-            store.remove(testObject._id, (err, removedObj) => {
+        return store.save(testObject)
+            .then(_savedObject => {
+                savedObject = _savedObject;
+                return store.remove(savedObject._id);
+            })
+            .then( removedObj => {
                 assert.deepEqual(removedObj, { removed: true } );
-                done();
+                return store.get(savedObject._id);
+            })
+            .then( savedObject => {
+                assert.strictEqual(savedObject, null);
             });
-
-        });
     });
 
-    it('gets all objects', done => {
-        const testObject = {name: 'Zac'};
-
-        store.save(testObject, (err, savedObject) => {
-            if(err) return done(err);
-            assert.ok(savedObject._id);
-            assert.equal(savedObject.name, testObject.name);
-
-            store.getAll((err, files) => {
-                if(err) return done(err);
-                assert.equal(files.length, 1);
-                done();
+    it('returns false when no id', () => {
+        return store.remove('bad id')
+            .then( status => {
+                assert.deepEqual( status, { removed: false });
             });
-        });
+    });
+
+    it('returns empty array', () => {
+        return store.getAll()
+            .then(items => {
+                assert.deepEqual(items, []);
+            });
+    });
+
+    it('gets all objects', () => {
+        const testObjects = [
+            {name: 'Zac'},
+            {name: 'Sandy'},
+            {name: 'Ted'}
+        ];
+        let savedObjects = null;
+        return Promise.all(testObjects.map(item => store.save(item)))
+            .then(_savedObjects => {
+                savedObjects = _savedObjects;
+                savedObjects.sort((x,y) => x._id < y._id ? -1 : 1);
+                return store.getAll();
+            })
+            .then(gotAll => {
+                assert.deepEqual(gotAll, savedObjects);
+            });
     });
 });
