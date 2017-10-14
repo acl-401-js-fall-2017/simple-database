652978730e2af76abe414f8636e0fb747a8ae6b7
mugworks <greenwood.pdx@gmail.com>
2017-10-13 17:51:05 -0700

    created promises on creating store instances

--- test/store.test.js
+++ test/store.test.js
@@ -5,9 +5,10 @@ const path = require('path');
 const rootDirectory = path.join(__dirname, 'data');
 const DB = new MakeDB(rootDirectory);
 const rimraf = promisify(require('rimraf'));
-// const fs = require('fs');
+const {readdir} = require('../lib/fsp');
+const Store = require('../lib/store');
 
-//Gives store and testObject global scope. 
+
 
 describe('make dB and stores', () => {
     let store = null;
@@ -36,18 +37,18 @@ describe('make dB and stores', () => {
     });
                 
                 
-    let testObject = {
-        name: 'dog'
-    };
-    let testObject2 = {
-        name: 'cat'
-    };
-    let testObject3 = {
-        name: 'horse'
-    };
+    // let testObject = {
+    //     name: 'dog'
+    // };
+    // let testObject2 = {
+    //     name: 'cat'
+    // };
+    // let testObject3 = {
+    //     name: 'horse'
+    // };
                 
     it('should get saved object', () => {
-        let testObject = {
+        const testObject = {
             name: 'dog'
         };
         return store.save(testObject) 
@@ -88,6 +89,9 @@ describe('make dB and stores', () => {
 
     it('should remove object with id', () => {
         let id = '';
+        let testObject = {
+            name: 'dog'
+        };
         return store.save(testObject)
             .then ((testObject) => {
                 id = testObject._id;
@@ -128,7 +132,7 @@ describe('make dB and stores', () => {
     });
 
 
-    it.only('getAll() should return an empty array for a newly created store', () => {
+    it('getAll() should return an empty array for a newly created store', () => {
         // let data = [];
         return store.getAll()
             .then ((data) => {
@@ -144,40 +148,25 @@ describe('make dB and stores', () => {
     //     });
     // });
 
-    it('should return an array of all objects in the directoy.', done => {
-        const testObjects = [
-            {name: 'dog'},  
-            {name: 'cat'},
-            {name: 'horse'}
-        ];
-        let allData = [];
-        Promise.all(testObjects.map(obj => store.save(obj))
-        
-        store.save(testObject, (err, data)=>{
             
 
-    it('should return an array of all objects in the directoy.', done => {
+    it('should return an array of all objects in the directoy.', () => {
         const testObjects = [
             {name: 'dog'},  
             {name: 'cat'},
             {name: 'horse'}
         ];
-        let savedData = [];
-        Promise.all(testObjects.map(obj => store.save(obj))
-            .then((obj) => {
-                savedData = data.map(obj => obj.name);
-            }
-            
-        
-        
-                    store.getAll((data, err) =>{
-                        if (err) return done(err);
-                        assert.deepEqual(sortedData.sort(), [savedObj1.name, savedObj2.name, savedObj3.name].sort());
-                        done();
-                    });
-                });               
+        let savedData = null;
+        return Promise.all(testObjects.map(obj => store.save(obj)))
+            .then((_savedData) => {
+                savedData = _savedData;
+                savedData = savedData.map(obj => obj.name);
+                return store.getAll();
+            })
+            .then ((all) => {
+                all = all.map(obj => obj.name);
+                assert.deepEqual(all.sort(), savedData.sort());
             });
-        });
     });
 
     // store.save(testObject, (err, data)=>{
@@ -204,29 +193,26 @@ describe('make dB and stores', () => {
 
 
 
-    it('getStore should return an instance of the store', done => {
-        DB.getStore('animals', (err, data) => {
-            if (err) return done(err);
-            let getStoreOutput = data;
-            fs.readdir(getStoreOutput.directory, (err, data) => {
-                if(err) return done(err);
-                assert.deepEqual(data, []);
-                assert.deepEqual(getStoreOutput.directory, path.join(rootDirectory, 'animals'));
-                done();
+    it('getStore should return an instance of the store', () => {
+        return DB.getStore('animals')
+            .then((newStore) => {
+                assert(newStore instanceof Store);
+                assert.deepEqual(newStore.directory, path.join(rootDirectory, 'animals'));
             });
-        });
     });
 
+    
+    
 
-    it('should create two store instances on unique paths', (done) => {
-        DB.getStore('michele', (err, data) => {  //eslint-disable-line
-            if(err) return done(err);
-            fs.readdir(DB.rootDir, (err, data) => {
-                if(err) return done(err);
-                let sortedPathNames = data.sort();
-                assert.deepEqual(sortedPathNames, ['michele', 'testerSHane']);
-                done();
+
+    it('should create two store instances on unique paths', () => {
+        return DB.getStore('michele')
+            .then(() => {
+                return readdir(DB.rootDir)
+                    .then((dirNames) => {
+                        assert.deepEqual(dirNames, ['michele', 'testerSHane']);
+                    });
             });
-        });
     });
 });
+
